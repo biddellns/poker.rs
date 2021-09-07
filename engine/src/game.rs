b6ca314d456c49e1a99c96c8b032d7057d5512a2
Nic Biddell <nicbiddell@pm.me>
2021-09-07 17:03:18 -0400

    Rest on print impl for now

--- engine/src/game.rs
+++ engine/src/game.rs
@@ -6,12 +6,12 @@ impl Display for Game {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         let line1 = " -------------------------------------";
         let line2 = "/                                      \\";
-        let line3a = "|                                       |";
         let line3 = "|                                      |";
-        let line4 = "\\                                     /";
-        let line5 = " -------------------------------------";
+        let line4 = "|                                      |";
+        let line5 = "\\                                     /";
+        let line6 = " -------------------------------------";
 
-        write!(f, "{}\n{}\n{}\n{}\n{}\n{}", line1, line2, line3a, line3, line4, line5)
+        write!(f, "{}\n{}\n{}\n{}\n{}\n{}", line1, line2, line3, line4, line5, line6)
     }
 }
 
