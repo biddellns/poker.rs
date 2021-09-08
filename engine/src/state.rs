d7cfd6eeed5967f5f4c0379ca533f6710d2eb99c
Nic Biddell <nicbiddell@pm.me>
2021-09-08 10:58:42 -0400

    Add initial state file

--- engine/src/state.rs
+++ engine/src/state.rs
@@ -10,10 +10,65 @@
 // 9. Eval cards for winner
 //10. DONE
 
+use crate::cards::Deck;
+use std::marker::PhantomData;
+
+struct TexasHoldemHand<S: State> {
+
+    deck: Deck,
+    // state: S,
+    marker: std::marker::PhantomData<S>
+}
+
+struct Start {}
+struct Shuffled {}
+struct DealtToPlayers {}
+struct DealtFlop {}
+struct DealtRiver {}
+struct DealTurn {}
+struct EvalWinner {}
+
+trait State {}
+impl State for Start {}
+impl State for Shuffled {}
+impl State for DealtToPlayers {}
+
+impl TexasHoldemHand<Start> {
+    fn new() -> TexasHoldemHand<Start> {
+        TexasHoldemHand {
+            deck: Deck::new(),
+            marker: PhantomData::<Start>,
+        }
+    }
+
+    fn shuffle(&mut self) -> TexasHoldemHand<Shuffled> {
+        self.deck.shuffle();
+
+        TexasHoldemHand {
+            deck: self.deck,
+            marker: PhantomData::<Shuffled>
+        }
+    }
+}
+
+
+
+trait Bet {
+    fn raise(amount: u8);
+    fn call();
+    fn check();
+    fn fold();
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::state::TexasHoldemHand;
+
     #[test]
     fn typestate_stuff() {
+        let hand = TexasHoldemHand::new().shuffle();
+        hand.shuffle();
+        hand.shuffle();
 
     }
 }
